5ff94ba96e1ac37457355bc1220222ca8ed19685
cokoyoh <charlesokoyoh@gmail.com>
2019-10-08 13:23:09 +0300

    Add a simple splash page using tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,62 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100">
+<body>
 <div id="app">
-    <nav class="bg-white">
-        <div class="container mx-auto">
-            <div class="flex justify-between items-center py-2">
-                <h1>
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                        {{--<img src="/images/logo.svg" alt="Birdboard ">--}}
-                    </a>
-                </h1>
-
-
-                <div class="">
-                    <!-- Right Side Of Navbar -->
-{{--                    <div class="navbar-nav ml-auto list-none">--}}
-{{--                        <!-- Authentication Links -->--}}
-{{--                        @guest--}}
-{{--                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-{{--                            @if (Route::has('register'))--}}
-{{--                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
-{{--                            @endif--}}
-{{--                        @else--}}
-
-
-{{--                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
-{{--                                <form id="logout-form" action="{{ route('logout') }}" method="POST"--}}
-{{--                                      style="display: none;">--}}
-{{--                                    @csrf--}}
-{{--                                </form>--}}
-{{--                            </div>--}}
-{{--                        @endguest--}}
-{{--                    </div>--}}
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <main class="container mx-auto py-4">
+    <div class="bg-gray-100">
         @yield('content')
-    </main>
+    </div>
 </div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center">
-        <button class="bg-white text-sm">TRial Button</button>
+    <div class="py-8 px-12">
+        <img class="h-10" src="/img/logo.svg" alt="Workcation">
+        <img class="mt-6 rounded-lg shadow-xl" src="/img/beach-work.jpg" alt="Woman workactioning on the beach">
+        <h1 class="mt-6 text-2xl font-bold text-gray-900 leading-tight">
+            You can work from anywhere. <span class="text-indigo-500">Take advantage of it</span>
+        </h1>
+
+        <p class="mt-2 text-gray-600">
+            Workaction helps you find work-friendly rentals in beautiful locations so you can enjoy some warm weather even when you are on vacation.
+        </p>
+
+        <div class="mt-4">
+            <a href="#"
+               class="inline-block bg-indigo-500 text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold text-sm"
+            >
+                Book your escape
+            </a>
+        </div>
     </div>
 @endsection
