1a5f1e34ddd5c2ed1cb052fe79844bcc7fb1d7b4
cokoyoh <charlesokoyoh@gmail.com>
2019-10-08 15:56:07 +0300

    Hover, focus and active classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
         <div class="py-8 px-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full lg:w-1/2 lg:py-24 lg:px-12">
             <div class="xl:max-w-lg xl:ml-auto">
                 <img class="h-10" src="/img/logo.svg" alt="Workcation">
+
                 <img class="mt-6 sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center rounded-lg shadow-xl lg:hidden"
                      src="/img/beach-work.jpg"
                      alt="Woman workactioning on the beach">
+
                 <h1 class="mt-6 text-2xl sm:text-4xl lg:text-3xl xl:text-4xl sm:mt-8 font-bold text-gray-900 leading-tight">
                     You can work from anywhere.
                     <br class="hidden lg:inline"><span class="text-indigo-500">Take advantage of it</span>
@@ -19,11 +21,12 @@
 
                 <div class="mt-4 sm:mt-6">
                     <a href="#"
-                       class="inline-block bg-indigo-500 text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold text-sm sm:text-base"
+                       class="inline-block bg-indigo-500 hover:bg-indigo-400 md:hover:bg-green-400 active:bg-indigo-600 focus:outline-none focus:shadow-outline text-white px-5 py-3 rounded-lg shadow-lg uppercase tracking-wider font-semibold text-sm sm:text-base"
                     >
                         Book your escape
                     </a>
                 </div>
+
             </div>
         </div>
 
